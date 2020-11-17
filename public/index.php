649a558f5185dfcdede6cb76c609308c53aefc06
SashaPlavi <sasha.bingulac@gmail.com>
2020-11-17 23:12:54 +0100

    routing :)

--- public/index.php
+++ public/index.php
@@ -1,10 +1,29 @@
  <?php
 
-  //echo $_SERVER['QUERY_STRING'];
+  $url = '/' . $_SERVER['QUERY_STRING'];
 
   require '../core/Router.php';
   $router = new Router();
-  echo get_class($router);
+  //echo get_class($router);
+
+  $router->add('/', ['controller' => 'Home', 'action' => 'index']);
+  $router->add('/posts', ['controller' => 'Posts', 'action' => 'index']);
+  $router->add('/posts/new', ['controller' => 'Posts', 'acction' => 'new']);
+
+  // echo '<pre>';
+  // var_dump($router->getRoutes());
+  // echo '<pre>';
+
+  if ($router->match($url)) {
+    // echo $url;
+    echo '<pre>';
+    var_dump($router->getParams());
+    echo '<pre>';
+  } else {
+    echo '  no route is find';
+  }
+
+
   ?>
 
 
