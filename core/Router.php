528928173cf3d9bb7c1fc69ea39c37163cb5fc5d
SashaPlavi <sasha.bingulac@gmail.com>
2020-11-21 14:59:15 +0100

    routing???

--- core/Router.php
+++ core/Router.php
@@ -6,8 +6,17 @@ class Router
   protected $routes = [];
   protected $params = [];
 
-  public function add($route, $params)
+  public function add($route, $params = [])
   {
+    // echo $route . '<br>';
+    $route = preg_replace('/\//', '\\/', $route);
+    // echo $route . '<br>';
+
+    $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+    //echo $route . '<br>';
+
+    $route = '/^' . $route . '$/i';
+    //echo $route . '<br>';
 
     $this->routes[$route] = $params;
   }
@@ -19,15 +28,30 @@ class Router
 
   public function match($url)
   {
-    echo $url;
+    // echo $url;
+    // foreach ($this->routes as $route => $params) {
+
+    //   if ($url == $route) {
+    //     $this->params = $params;
+    //     return true;
+    //   }
+    // }
+    // return false;
+    // $reg_ex = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/ ";
     foreach ($this->routes as $route => $params) {
 
-      if ($url == $route) {
-        $this->params = $params;
-        return true;
+      if (preg_match($route, $url, $matches)) {
+
+        $params = [];
+        foreach ($matches as $key => $match) {
+          if (is_string($key)) {
+            $params[$key] = $match;
+          }
+        }
       }
     }
-    return false;
+    $this->params = $params;
+    return true;
   }
 
   public function getParams()
--- public/index.php
+++ public/index.php
@@ -1,27 +1,30 @@
  <?php
 
-  $url = '/' . $_SERVER['QUERY_STRING'];
+  $url =    $_SERVER['QUERY_STRING'];
 
   require '../core/Router.php';
   $router = new Router();
   //echo get_class($router);
 
+
   $router->add('/', ['controller' => 'Home', 'action' => 'index']);
   $router->add('/posts', ['controller' => 'Posts', 'action' => 'index']);
-  $router->add('/posts/new', ['controller' => 'Posts', 'acction' => 'new']);
+  //$router->add('/posts/new', ['controller' => 'Posts', 'acction' => 'new']);
+  $router->add('{controller}/{action}');
+  // $router->add('admin/{action}/{controller}');
 
-  // echo '<pre>';
-  // var_dump($router->getRoutes());
-  // echo '<pre>';
+  echo '<pre>';
+  var_dump($router->getRoutes());
+  echo '<pre>';
 
-  if ($router->match($url)) {
-    // echo $url;
-    echo '<pre>';
-    var_dump($router->getParams());
-    echo '<pre>';
-  } else {
-    echo '  no route is find';
-  }
+  // if ($router->match($url)) {
+  //   // echo $url;
+  //   echo '<pre>';
+  //   var_dump($router->getParams());
+  //   echo '<pre>';
+  // } else {
+  //   echo '  no route is find';
+  // }
 
 
   ?>
